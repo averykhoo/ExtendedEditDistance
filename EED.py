a8a8dca87b43b6a31b538ffef177d8e47be842e7
averykhoo <averykhoo@gmail.com>
2019-09-22 11:14:13 +0800

    remove old python eed

--- EED.py
+++ EED.py
@@ -51,7 +51,7 @@ def eed(hyp, ref, deletion=0.2, insertion=1.0, substitution=1.0, jump=2.0, rho=0
     debug_idx = []  # indices of matched string
 
     # coverage: count how many times each char is visited
-    visit_coverage = [0] * (len(hyp) + 1)
+    visit_coverage = [0.0] * (len(hyp) + 1)
 
     # the i-th row stores cost of cheapest path from (0,0) to (i,l) in CDER alignment grid
     row = [0.0] + [1.0] * len(hyp)  # CDER initial row
@@ -72,7 +72,7 @@ def eed(hyp, ref, deletion=0.2, insertion=1.0, substitution=1.0, jump=2.0, rho=0
         min_cost, min_cost_idx = min((cost, idx) for idx, cost in enumerate(next_row))
 
         # increment the visit count
-        visit_coverage[min_cost_idx] += 1
+        visit_coverage[min_cost_idx] += 1.0
 
         # long jump allowed only if ref char is whitespace
         # the original algo only checks if ord(ref_char) == 32 (ascii space)
@@ -90,7 +90,7 @@ def eed(hyp, ref, deletion=0.2, insertion=1.0, substitution=1.0, jump=2.0, rho=0
 
     # overall error == final cell of final row
     errors = row[-1]
-    weighted_coverage = rho * sum(abs(num_visits - 1) for num_visits in visit_coverage)
+    weighted_coverage = rho * sum(abs(num_visits - 1.0) for num_visits in visit_coverage)
     result = (errors + weighted_coverage) / (len(ref) + weighted_coverage)
 
     # debug
@@ -104,49 +104,6 @@ def eed(hyp, ref, deletion=0.2, insertion=1.0, substitution=1.0, jump=2.0, rho=0
         pass
 
     return min(1.0, result)
-
-
-# Distance measure used for substitutions/identity operation
-def distance(refWord, hypWord):
-    return 0 if refWord == hypWord else 1
-
-
-# Python Implementation of EED, ~30x slower than the C++ one
-def eed_python(hyp, ref):
-    hyp.insert(0, " ")
-    hyp.append(" ")
-    ref.insert(0, " ")
-    ref.append(" ")
-
-    alpha = 2.0
-    deletion = 0.2
-    # substitutions are implemented via the distance function
-    insertion = 1.0
-    rho = 0.3
-
-    lj = [0] * (len(hyp) + 1)
-    row = [1] * (len(hyp) + 1)  # row[i] stores cost of cheapest path from (0,0) to (i,l) in CDER aligment grid.
-    row[0] = 0  # CDER initialisation 0,0 = 0 rest 1
-    nextRow = [float('inf')] * (len(hyp) + 1)
-    for w in range(1, len(ref) + 1):
-        for i in range(0, len(hyp) + 1):
-            if i > 0:
-                nextRow[i] = min(nextRow[i - 1] + deletion, row[i - 1] + distance(ref[w - 1], hyp[i - 1]),
-                                 row[i] + insertion)
-            else:
-                nextRow[i] = row[i] + 1.0
-        minInd = nextRow.index(min(nextRow))
-        lj[minInd] += 1
-        # Long Jumps
-        if ref[w - 1] == " ":
-            longJump = alpha + nextRow[minInd]
-            nextRow = [x if x < longJump else longJump for x in nextRow]
-        row = nextRow
-        nextRow = [float('inf')] * (len(hyp) + 1)
-
-    coverage = rho * sum([x if x > 1 else 0 for x in lj])
-
-    return min(1, (row[-1] + coverage) / (float(len(ref)) + coverage))
 
 
 # Provides System scoring with full preprocessing in the case where EED is used as an import
@@ -200,17 +157,11 @@ if __name__ == '__main__':
     hyp_sent = 'The relationship between Obama and Netanyahu has been strained for years.'
     ref_sent = 'Relations between Obama and Netanyahu have been strained for years.'
 
-    print(eed_python(list(hyp_sent), list(ref_sent)))
-    print(eed_python(list(ref_sent), list(hyp_sent)))
-
     print(eed(hyp_sent, ref_sent))
     print(eed(ref_sent, hyp_sent))
 
     hyp_sent = 'super calloused fragile mystic hexed by haliotosis'
     ref_sent = 'supercalifragilisticexpialidocious'
-
-    print(eed_python(list(hyp_sent), list(ref_sent)))
-    print(eed_python(list(ref_sent), list(hyp_sent)))
 
     print(eed(hyp_sent, ref_sent))
     print(eed(ref_sent, hyp_sent))
@@ -219,4 +170,3 @@ if __name__ == '__main__':
     ref_sent = 'hello world asdfg say qwerty'
 
     print(eed(hyp_sent, ref_sent))
-    print(eed_python(list(hyp_sent), list(ref_sent)))
